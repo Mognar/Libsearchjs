d6c4be5990239cd8dd919197f8469256f2b20ecf
Mognar <philipbgorman@gmail.com>
2020-08-10 16:36:01 +0100

    Update and rename customfooter3.js to customfooter4.js

--- customfooter3.js
+++
@@ -1,1 +0,0 @@
-$('.queryBox.ng-scope').append("<footer><p><a href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a></p></footer>'");
---
+++ customfooter4.js
@@ -0,0 +1,1 @@
+$('.queryBox.ng-scope').append("<p><a href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a></p>'");
