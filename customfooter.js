367e51103d5d7a074ad61a72fcc5faebc40ed0cb
Mognar <philipbgorman@gmail.com>
2020-08-10 15:38:11 +0100

    Update customfooter.js

--- customfooter.js
+++ customfooter.js
@@ -1,1 +1,1 @@
-$('footer').append("<footer><p><a href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a></p></footer>'");
+$('.footer').append("<footer><p><a href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a></p></footer>'");
