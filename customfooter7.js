35e6fab728937b3a026ddf134e071a18f10e194e
Mognar <philipbgorman@gmail.com>
2020-08-10 17:12:50 +0100

    Update and rename customfooter7.js to customfooter8.js

--- customfooter7.js
+++
@@ -1,1 +0,0 @@
-$('.footer').append("<a ng-href='https://www.parliament.uk/site-information/accessibility/' href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a>'");
