bd29670a7ebe63649bf2d2f0c759a785067f02e1
Mognar <philipbgorman@gmail.com>
2020-09-01 14:22:43 +0100

    Update customfooter9.js

--- customfooter9.js
+++ customfooter9.js
@@ -1,1 +1,4 @@
+let home = document.querySelector('.home');
+home.style.height = '98%';
+
 $('.footer').append("<a ng-href='https://www.parliament.uk/site-information/accessibility/' href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a>");
