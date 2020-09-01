dc9072fdbb417b6d5043709f2267e7fe11fac2b0
Mognar <philipbgorman@gmail.com>
2020-09-01 14:34:41 +0100

    Update and rename customfooter10.js to customfooter11.js

--- customfooter11.js
+++ customfooter11.js
@@ -1,4 +1,3 @@
-let home = document.querySelector('.home');
-home.style.height = '98%';
+document.body.style.height = '98%'
 
 $('.footer').append("<a ng-href='https://www.parliament.uk/site-information/accessibility/' href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a>");
