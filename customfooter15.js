3527c99a624ed72affc546ede48de4f6d105d693
Mognar <philipbgorman@gmail.com>
2020-09-03 10:36:56 +0100

    Update and rename customfooter14.js to customfooter15.js

--- customfooter15.js
+++ customfooter15.js
@@ -1,4 +1,4 @@
-if (window.location.href == "https://parliament.preview.summon.serialssolutions.com/#!/") {
+if (document.location == "https://parliament.preview.summon.serialssolutions.com/#!/") {
 document.body.style.height = '98%'
 
 $('.footer').append("<a ng-href='https://www.parliament.uk/site-information/accessibility/' href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a>");
