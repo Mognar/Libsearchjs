4cdee4455f50790b9898cc31149e29d5722e1b52
Mognar <philipbgorman@gmail.com>
2020-09-03 11:13:32 +0100

    Update and rename customfooter18.js to customfooter19.js

--- customfooter19.js
+++ customfooter19.js
@@ -1,6 +1,8 @@
 $(document).ready( function () {
-str = document.title;
-if (document.location.href == "https://parliament.preview.summon.serialssolutions.com/#!/" && str.startsWith("Home")) {
+//str = document.title;
+//if (document.location.href == "https://parliament.preview.summon.serialssolutions.com/#!/" && str.startsWith("Home")) {
+str = document.location.href;  
+if (!str.includes("search?")) {
 document.body.style.height = '98%';
 
 $('.footer').append("<a ng-href='https://www.parliament.uk/site-information/accessibility/' href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a>");
