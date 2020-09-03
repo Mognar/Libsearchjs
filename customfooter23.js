42518e08fff1e11b22cde9937185ac8950f418fd
Mognar <philipbgorman@gmail.com>
2020-09-03 22:09:08 +0100

    Update and rename customfooter22.js to customfooter23.js

--- customfooter23.js
+++ customfooter23.js
@@ -1,4 +1,3 @@
-$(document).ready( function () {
 //str = document.title;
 //if (document.location.href == "https://parliament.preview.summon.serialssolutions.com/#!/" && str.startsWith("Home")) {
 //str = document.location.href;  
@@ -7,4 +6,3 @@ document.body.style.height = '98%';
 
 $('.footer').append("<a ng-href='https://www.parliament.uk/site-information/accessibility/' ng-hide=\"view == 'search'\" href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a>");
 //};
-});
