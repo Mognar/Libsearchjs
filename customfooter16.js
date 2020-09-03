5245d4d4f9cac6d785c70ace626a173bb84d16ad
Mognar <philipbgorman@gmail.com>
2020-09-03 10:54:49 +0100

    Update customfooter16.js

--- customfooter16.js
+++ customfooter16.js
@@ -1,5 +1,5 @@
 str = document.title;
-if (document.location == "https://parliament.preview.summon.serialssolutions.com/#!/" && str.startsWith("Home")) {
+if (document.location.href == "https://parliament.preview.summon.serialssolutions.com/#!/" && str.startsWith("Home")) {
 document.body.style.height = '98%';
 
 $('.footer').append("<a ng-href='https://www.parliament.uk/site-information/accessibility/' href='https://www.parliament.uk/site-information/accessibility/'>Accessibility Statement</a>");
